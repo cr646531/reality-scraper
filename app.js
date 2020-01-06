74fe134812054113eed30f865d388e90f96ed5e7
Charlie <cr646531@gmail.com>
2020-01-06 14:19:41 -0500

    testing: traverses 20 links deep

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cheerio = require("cheerio");
 
 const siteUrl = "https://en.wikipedia.org/wiki/Reality";
 
-var nextUrl;
+//var nextUrl;
 
 
 const port = process.env.PORT || 3000;
@@ -25,8 +25,6 @@ app.get('/', async (req, res) => {
     var $ = await fetchData(siteUrl);
 
     var links = $('a', '.mw-parser-output');
-
-    // console.log(links.length);
 
     for(var i = 0; i < 20; i++){
         // if the link has a class, it's a redirect
@@ -49,7 +47,8 @@ app.get('/', async (req, res) => {
         $ = await fetchData(nextUrl);
         links = $('a', '.mw-parser-output');
 
-        for(var i = 0; i < 20; i++){
+        for(var i = 0; i < 5; i++){
+
             // if the link has a class, it's a redirect
             if(!links[i].attribs.class) {
                 // if the link has no title, it's a citation
@@ -68,4 +67,59 @@ app.get('/', async (req, res) => {
     res.send('test');
 
     //redirect to new page with the next async request
+});
+
+
+
+
+app.get('/philosophy', async (req, res) => {
+    console.log('Fetching data');
+
+    var output = '';
+    var counter = 0;
+    var url = 'https://en.wikipedia.org/wiki/Object_(philosophy)';
+
+
+    // for testing purposes, only go 5 pages deep
+    while(counter < 20) {
+
+        // grab the html for the page
+        var $ = await fetchData(url);
+
+        // grab an array of all the paragraphs on the page - this is where we want to search for links
+        var paragraphs = $('p', '.mw-parser-output');
+        var links = [];
+
+        // go through paragraphs and find the children of each
+        for(var i = 0; i < paragraphs.length; i++){
+
+            // go through children and find the links of each
+            var children = paragraphs[i].children;
+            for(var j = 0; j < children.length; j++){
+
+                // if the link does NOT go to a definition or link to section of the page - add it to the list
+                var curr = children[j];
+                if(curr.name == 'a') {
+
+                    // definitions begin with "wikt" in their title -> for example: 'wikt:entity' would lead to a definition
+                    if(curr.attribs.title){
+                        if(curr.attribs.title.indexOf('wikt') == -1){
+                            console.log(curr.attribs.href);
+                            links.push([curr.attribs.title, curr.attribs.href]);
+                        }
+                    } 
+                }
+            }
+        }
+
+        console.log('\n\n\n\n');
+
+        output += `${links[0][0]} ------> `;
+
+        // visit the first link and repeat
+        url = 'https://en.wikipedia.org' + links[0][1];
+        counter++;
+    }
+
+    res.send(output);
 });
